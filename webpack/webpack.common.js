d3ba1631eaa4b8bd27302f7507faf92ec4e6c5fb
psdaniel <daniel.n10@hotmail.com>
2021-06-27 09:55:44 -0300

    chore: last configs

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -39,5 +40,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: "source", to: "dest" }
+      ]
+    })
   ],
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,13 @@
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('Application')
-    })
+    }),
+    new BundleAnalyzerPlugin(),
   ]
 }
